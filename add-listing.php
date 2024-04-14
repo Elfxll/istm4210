4790ca946b213ac16fcdcb30d1283bb12a9c2987
mnguyen1030 <62580830+mnguyen1030@users.noreply.github.com>
2024-04-14 15:09:06 -0400

    started and setup user listings page template
    also debugged add-listing.php

--- add-listing.php
+++ add-listing.php
@@ -9,9 +9,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Create connection
     $conn = new mysqli($servername, $username, $password, $database);
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
